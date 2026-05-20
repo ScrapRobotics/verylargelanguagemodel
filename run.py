444b1ea317cad8549974d456c95e747ca0e09da1
Lonny Sui <lonnysui3@gmail.com>
2026-05-20 11:09:29 -0500

    Made it not add spaces before periods

--- run.py
+++ run.py
@@ -4,21 +4,33 @@ import os
 import random
 from rich import print
 from pathlib import Path
+def smartTitle(word):
+    if(word.title()==word):
+        return word
+    temp = ""
+    for x in range(len(word)):
+        if x==0:
+            temp+=word[x].capitalize()
+        else:
+            temp+=word[x]
+    return temp
 os.chdir(sys.path[0])
 with open(Path('data.json'), "r") as our_file:
     training = json.loads(our_file.read())
 prompt = input("ScrapGPT Will Fill In Your Sentences . Be Sure To Format It Like This : ")
 length = int(input("How many sentences? "))
-starterword = prompt.split()[-1].title()
-current = starterword
+starterword = smartTitle(prompt.split()[-1])
+current = random.choice(training[starterword])
 counter = 0
-print(prompt, end=" ")
+print(prompt, end="")
 while counter < length:
+    if(current not in "!?."):
+        print(" "+current,end="")
+    else:
+        print(current,end="")
+        counter+=1
     if current not in training.keys():
         print("[red]***ENCOUNTERED WORD NOT IN DATA***")
         break
-    next = random.choice(training[current])
-    print(next, end=" ")
-    current = next
-    if next in "!?.":
-        counter+=1
+    current = random.choice(training[current])
+
